3e6c0d5ef44de16fb9367d3f8b4822937966da4f
simonmcho <simonmcho@gmail.com>
2017-02-05 20:41:06 -0800

    updating main.java and main.class

--- COMP1451_JUnitQUIZ/src/Main.java
+++ COMP1451_JUnitQUIZ/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Book b = new Book(1999, "simon", "cho", "fuck");
+		Book b = new Book(1999, "simon", "cho", "simon's book");
 		
 	}
 
